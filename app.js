75fd14919160ceb12a0c36d9e836c08f7a1659be
lilight <100maz100@gmail.com>
2026-02-04 15:10:36 +0200

    small app changes

--- app.js
+++ app.js
@@ -47,5 +47,5 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} updated`));
+app.listen(port, () => console.log("Listening on", port));
 
